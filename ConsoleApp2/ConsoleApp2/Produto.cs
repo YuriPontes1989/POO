5c7ba158acaa11090754092823a4e940df23fd45
YuriPontes1989 <69511939+YuriPontes1989@users.noreply.github.com>
2020-09-27 22:44:57 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Produto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade) //método construtor.
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+
+        public double ValorTotalEstoque() //calculo do valor de estoque.
+        {
+            return Preco * Quantidade;
+        }
+
+        public void Adiciona(int quantitade) //adiciona peças ao estoque.
+        {
+            Quantidade += quantitade;
+        }
+
+        public void Remover(int quantidade) //remove peças do estoque.
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString() //método de ToString para impressão de informações.
+        {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
